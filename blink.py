0b04a7036105ca43d0164de031bd9b9b37203de7
Eric Cioffi <ecioffi@usc.edu>
2018-02-27 17:08:16 -0800

    blink should read light and sound sensors now

--- blink.py
+++ blink.py
@@ -7,7 +7,7 @@ import time
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
-Hardware SPI configuration:
+# Hardware SPI configuration:
 SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
@@ -18,12 +18,6 @@ print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |
 print('-' * 57)
 # Main program loop.
 while True:
-    # Read all the ADC channel values in a list.
-    values = [0]*8
-    for i in range(8):
-        # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
-    # Print the ADC values.
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-    # Pause for half a second.
-    time.sleep(0.5)
+	print("Light sensor: ", mcp.read_adc(0))
+	print("Sound sensor: ", mcp.read_adc(1))
+	time.sleep(0.5)
