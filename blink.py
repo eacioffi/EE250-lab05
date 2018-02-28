5e917baec328e569d3c81563fc6ffb84fc8f821f
Eric Cioffi <ecioffi@usc.edu>
2018-02-28 01:53:16 +0000

    all code in place

--- blink.py
+++ blink.py
@@ -7,18 +7,52 @@ import time
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
+# GPIO config
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
 # Hardware SPI configuration:
 SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
-print('Reading MCP3008 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
-print('-' * 57)
-# Main program loop.
-
-GPIO.output(11, 1) # led on
+# Main loop
 while True:
-	print("Light sensor: ", mcp.read_adc(0), "Sound sensor: ", mcp.read_adc(1))
-	time.sleep(0.5)
+	for i in range(5):
+		GPIO.output(11, True)    # led on
+		time.sleep(0.5)	         # wait 500ms
+		GPIO.output(11, False)   # led off
+		time.sleep(0.5)	         # wait 500ms
+	
+	for i in range(50):
+		light = mcp.read_adc(1)
+		print("light sensor:", light)
+		if light < 300:
+			print("dark")
+		else:
+			print ("bright")
+		time.sleep(.1)
+	
+	for i in range(4):
+		GPIO.output(11, True)
+		time.sleep(.2)
+		GPIO.output(11, False)
+		time.sleep(.2)
+
+	for i in range(50):
+		sound = mcp.read_adc(0)
+		print("sound sensor:", sound)
+		if sound < 700:
+			GPIO.output(11, True)
+			time.sleep(.1)
+			GPIO.output(11, False)
+		else:
+			time.sleep(.1)
+	
+	for i in range(4):
+		GPIO.output(11, True)
+		time.sleep(.2)
+		GPIO.output(11, False)
+		time.sleep(.2)
